4120003f18b158ef0dca301f80546085917d6236
Jeyhun Farajov <97691925+jeyefendi@users.noreply.github.com>
2022-12-17 15:02:03 +0300

    component created with props

--- App.js
+++ App.js
@@ -1,35 +1,15 @@
 import { Text, View } from "react-native";
-import styled from "styled-components/native";
-
-const Post = styled.View`
-  padding: 15px;
-  border-bottom-width: 2px;
-  border-bottom-color: black;
-  border-bottom-style: solid;
-`;
-
-const PostImage = styled.Image`
-  width: 100px;
-  height: 100px;
-`;
+import { Card } from "./components/Card";
 
 export default function App() {
   return (
-    <View
-      style={{
-        height: "100%",
-        justifyContent: "center",
-        alignContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Post>
-        <PostImage
-          source={{
-            uri: "https://raw.githubusercontent.com/jeyefendi/jeyefendi/main/public/favicon.webp",
-          }}
-        />
-      </Post>
+    <View>
+      <Card
+        title="jeyefendi"
+        imageUrl={
+          "https://raw.githubusercontent.com/jeyefendi/jeyefendi/main/public/favicon.webp"
+        }
+      />
     </View>
   );
 }
---
+++ components/Card.jsx
@@ -0,0 +1,32 @@
+import styled from "styled-components/native";
+
+const CardView = styled.View`
+  flex-direction: row;
+  padding: 15px;
+  border-bottom-width: 2px;
+  border-bottom-color: black;
+  border-bottom-style: solid;
+`;
+
+const CardImage = styled.Image`
+  width: 100px;
+  height: 100px;
+  border-radius: 12px;
+  margin-right: 12px;
+`;
+
+const CardTitle = styled.Text`
+  font-size: 14px;
+  font-weight: 700;
+`;
+
+export const Card = ({ title, imageUrl, createdAt }) => {
+    return <CardView>
+        <CardImage
+          source={{
+            uri: imageUrl,
+          }}
+        />
+        <CardTitle>{title}</CardTitle>
+      </CardView>
+}
